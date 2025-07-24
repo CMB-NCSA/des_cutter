531fbe0eebc10e016743a625ecf95139b3e58093
Felipe Menanteau <felipe@illinois.edu>
2025-07-24 15:01:37 -0500

    Update to creation of parquet files.

--- duckdb/make_tables.sql
+++ duckdb/make_tables.sql
@@ -1,3 +1,29 @@
+-- We are using this one --
+-- Get FILESIZE and MD5SUM and CREATED_DATE using desoper as DESFILE does not exists on dessci
+-- create table felipe.Y6A2_COADD_FILEPATH as
+  select c.FILENAME, c.TILENAME, c.BAND, c.FILETYPE, f.PATH, f.COMPRESSION, d.CREATED_DATE, d.FILESIZE, d.MD5SUM
+  from prod.COADD c, prod.PROCTAG, prod.FILE_ARCHIVE_INFO f, prod.DESFILE d
+         where prod.PROCTAG.TAG='Y6A2_COADD'
+--         and c.FILETYPE='coadd'
+           and c.PFW_ATTEMPT_ID=prod.PROCTAG.PFW_ATTEMPT_ID
+           and f.FILENAME=c.FILENAME
+           and c.FILENAME=d.FILENAME
+           and f.COMPRESSION=d.COMPRESSION;
+
+--- build table with info for finalcut files
+select i.FILENAME, f.PATH, i.BAND, i.EXPTIME, i.AIRMASS, i.FWHM, i.NITE, i.EXPNUM, i.CCDNUM, e.DATE_OBS, e.MJD_OBS,
+       d.CREATED_DATE, d.FILESIZE, d.MD5SUM,
+       i.CROSSRA0, i.RACMIN, i.RACMAX, i.DECCMIN, i.DECCMAX,
+       i.RAC1, i.RAC2, i.RAC3, i.RAC4, i.DECC1, i.DECC2, i.DECC3, i.DECC4
+from prod.IMAGE i, prod.EXPOSURE e, prod.FILE_ARCHIVE_INFO f, prod.PROCTAG, prod.DESFILE d
+      where prod.PROCTAG.TAG='Y6A1_FINALCUT'
+        and i.EXPNUM=e.EXPNUM
+        and i.FILENAME=f.FILENAME
+        and i.FILENAME=d.FILENAME
+        and i.FILETYPE='red_immask'
+        and f.COMPRESSION=d.COMPRESSION;
+
+
 -- To create a new table on dessci for Y6A2 called felipe.Y6A2_COADD_FILEPATH:
 -- DROP TABLE felipe.Y6A2_COADD_FILEPATH
 create table Y6A2_COADD_FILEPATH as
@@ -10,15 +36,19 @@ create table Y6A2_COADD_FILEPATH as
 create table Y6A2_COADD_FILEPATH as
   select c.FILENAME, c.TILENAME, c.BAND, f.PATH, f.COMPRESSION
   from prod.COADD c, prod.PROCTAG, prod.FILE_ARCHIVE_INFO f
-         where prod.PROCTAG.TAG='Y6A2_COADD' and
---           c.FILETYPE='coadd' and
-           c.PFW_ATTEMPT_ID=PROCTAG.PFW_ATTEMPT_ID and des_admin.Y6A2
-           f.FILENAME=c.FILENAME;
+         where prod.PROCTAG.TAG='Y6A2_COADD'
+--         and c.FILETYPE='coadd'
+           and c.PFW_ATTEMPT_ID=prod.PROCTAG.PFW_ATTEMPT_ID
+           and f.FILENAME=c.FILENAME;
 
 -- SELECT COUNT(*) FROM Y6A2_COADD_FILEPATH;
 -- COUNT(*)
 ----------
 -- 111859
+
+
+
+
 
 
 --- build table with info for finalcut files
